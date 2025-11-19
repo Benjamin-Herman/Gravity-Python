b97a8ad8390d32f6e3dccd4e07c6ed0efc8fba83
gwoth-design <benjaminaherman@gmail.com>
2025-11-19 16:43:05 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import pygame
 import sys
 
-# ----- INIT CODE - THE WINDOW ------
 pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+class Circle:
+    def __init__(self, x, y, r, color, mass):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.color = color
+        self.mass = mass
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
-#---- Circle pos, size, colour ------
-x = WIDTH // 2
-y = HEIGHT // 2
-r = 60
-color = (255, 120, 120)
+c1 = Circle(WIDTH//2, HEIGHT//2, 60, (255,120,120), 1)
+c2 = Circle(200, 150, 40, (120,255,120), 1)
+c3 = Circle(600, 450, 50, (120,120,255), 1)
 
 while True:
     for event in pygame.event.get():
-        #get input to quit and any others
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    #fill the screen to reset
     screen.fill((30, 30, 40))
-    #draw circle
-    pygame.draw.circle(screen, color, (x, y), r)
+
+    c1.draw(screen)
+    c2.draw(screen)
+    c3.draw(screen)
 
     pygame.display.flip()
